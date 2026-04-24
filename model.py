5c1afd07c8a0c1cc6af79d0e8056b1b9dcda8936
abreulucas7 <abreulucas7@email.com>
2026-04-24 19:24:23 -0300

    update

--- model.py
+++ model.py
@@ -1,138 +1,191 @@
 import sqlite3
 
+def conectar():
+    return sqlite3.connect("Acorde.db")
+
+
 def adicionar_musica(musica, artista, tablatura, letra):
-    with sqlite3.connect("Acorde.db") as conn:
-        sql_insert_info = '''
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
         INSERT INTO info (musica, artista, tablatura, letra)
         VALUES (?, ?, ?, ?)
-        '''
-        cur = conn.execute(sql_insert_info, (musica, artista, tablatura, letra))
-        return cur.lastrowid
+        """,
+        (musica, artista, tablatura, letra),
+    )
+    last_id = cursor.lastrowid
+    conn.commit()
+    conn.close()
+    return last_id
+
 
 def listar_musicas():
-    with sqlite3.connect("Acorde.db") as conn:
-        sql_listar_info = '''
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
         SELECT id, musica, artista, tablatura, letra
         FROM info
-        '''
-        cur = conn.cursor()        
-        cur.execute(sql_listar_info)        
-        return cur.fetchall() 
-    
+        """
+    )
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
 
 def listar_musicas_com_playlists():
-    with sqlite3.connect("Acorde.db") as conn:
-        cur = conn.execute(
-            """
-            SELECT i.id, i.musica, i.artista, i.tablatura, i.letra,
-                   GROUP_CONCAT(p.nomes, ', ') AS nomes_playlists
-            FROM info i
-            LEFT JOIN infoplaylists ip ON CAST(ip.IdFaixa AS INTEGER) = i.id
-            LEFT JOIN playlists p ON CAST(ip.IdPlaylist AS INTEGER) = p.id
-            GROUP BY i.id, i.musica, i.artista, i.tablatura, i.letra
-            ORDER BY i.musica COLLATE NOCASE
-            """
-        )
-        return cur.fetchall()
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        SELECT i.id, i.musica, i.artista, i.tablatura, i.letra,
+               GROUP_CONCAT(p.nomes, ', ') AS nomes_playlists
+        FROM info i
+        LEFT JOIN infoplaylists ip ON CAST(ip.IdFaixa AS INTEGER) = i.id
+        LEFT JOIN playlists p ON CAST(ip.IdPlaylist AS INTEGER) = p.id
+        GROUP BY i.id, i.musica, i.artista, i.tablatura, i.letra
+        ORDER BY i.musica COLLATE NOCASE
+        """
+    )
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
 
 
 def listar_playlists():
-    with sqlite3.connect("Acorde.db") as conn:
-        sql_listar_playlists = '''
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
         SELECT nomes, id
         FROM playlists
-        '''
-        cur = conn.cursor()        
-        cur.execute(sql_listar_playlists)        
-        return cur.fetchall() 
-    
+        """
+    )
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+
 def criar_playlist(nomes):
-    with sqlite3.connect("Acorde.db") as conn:
-        sql_criar_playlist = '''
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
         INSERT INTO playlists (nomes)
         VALUES (?)
-        '''
-        conn.execute(sql_criar_playlist, (nomes,))
+        """,
+        (nomes,),
+    )
+    conn.commit()
+    conn.close()
+
 
 def obter_playlist(id):
-    with sqlite3.connect("Acorde.db") as conn:
-        cur = conn.execute(
-            "SELECT id, nomes FROM playlists WHERE id = ?",
-            (id,),
-        )
-        return cur.fetchone()
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT id, nomes FROM playlists WHERE id = ?",
+        (id,),
+    )
+    row = cursor.fetchone()
+    conn.close()
+    return row
+
 
 def obter_faixa(id):
-    with sqlite3.connect("Acorde.db") as conn:
-        cur = conn.execute(
-            "SELECT id, musica FROM info WHERE id = ?",
-            (id,),
-        )
-        return cur.fetchone()
-    
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT id, musica FROM info WHERE id = ?",
+        (id,),
+    )
+    row = cursor.fetchone()
+    conn.close()
+    return row
 
 
 def listar_faixas_da_playlist(id_playlist):
-    with sqlite3.connect("Acorde.db") as conn:
-        cur = conn.execute(
-            """
-            SELECT i.id, i.musica, i.artista, i.tablatura, i.letra
-            FROM info i
-            INNER JOIN infoplaylists ip ON CAST(ip.IdFaixa AS INTEGER) = i.id
-            WHERE CAST(ip.IdPlaylist AS INTEGER) = ?
-            ORDER BY i.musica COLLATE NOCASE
-            """,
-            (id_playlist,),
-        )
-        return cur.fetchall()
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        SELECT i.id, i.musica, i.artista, i.tablatura, i.letra
+        FROM info i
+        INNER JOIN infoplaylists ip ON CAST(ip.IdFaixa AS INTEGER) = i.id
+        WHERE CAST(ip.IdPlaylist AS INTEGER) = ?
+        ORDER BY i.musica COLLATE NOCASE
+        """,
+        (id_playlist,),
+    )
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
 
 
 def adicionar_faixa_a_playlist(id_playlist, id_faixa):
     ps = str(id_playlist)
     fs = str(id_faixa)
-    with sqlite3.connect("Acorde.db") as conn:
-        cur = conn.execute(
-            "SELECT 1 FROM infoplaylists WHERE IdPlaylist = ? AND IdFaixa = ?",
-            (ps, fs),
-        )
-        if cur.fetchone():
-            return False
-        conn.execute(
-            "INSERT INTO infoplaylists (IdPlaylist, IdFaixa) VALUES (?, ?)",
-            (ps, fs),
-        )
-        return True
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT 1 FROM infoplaylists WHERE IdPlaylist = ? AND IdFaixa = ?",
+        (ps, fs),
+    )
+    if cursor.fetchone():
+        conn.close()
+        return False
+    cursor.execute(
+        "INSERT INTO infoplaylists (IdPlaylist, IdFaixa) VALUES (?, ?)",
+        (ps, fs),
+    )
+    conn.commit()
+    conn.close()
+    return True
 
 
 def remover_faixa_da_playlist(id_playlist, id_faixa):
-    with sqlite3.connect("Acorde.db") as conn:
-        conn.execute(
-            "DELETE FROM infoplaylists WHERE IdPlaylist = ? AND IdFaixa = ?",
-            (str(id_playlist), str(id_faixa)),
-        )
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        "DELETE FROM infoplaylists WHERE IdPlaylist = ? AND IdFaixa = ?",
+        (str(id_playlist), str(id_faixa)),
+    )
+    conn.commit()
+    conn.close()
 
 
 def excluir_playlists(id):
-    with sqlite3.connect("Acorde.db") as conn:
-        conn.execute(
-            "DELETE FROM infoplaylists WHERE IdPlaylist = ?",
-            (str(id),),
-        )
-        sql_excluir_playlists = '''
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        "DELETE FROM infoplaylists WHERE IdPlaylist = ?",
+        (str(id),),
+    )
+    cursor.execute(
+        """
         DELETE FROM playlists
         WHERE id = (?)
-        '''
-        conn.execute(sql_excluir_playlists, (id,))
+        """,
+        (id,),
+    )
+    conn.commit()
+    conn.close()
 
 
 def excluir_faixas(id):
-    with sqlite3.connect("Acorde.db") as conn:
-        conn.execute(
-            "DELETE FROM infoplaylists WHERE IdFaixa = ?",
-            (str(id),),
-        )
-        sql_excluir_faixas = '''
+    conn = conectar()
+    cursor = conn.cursor()
+    cursor.execute(
+        "DELETE FROM infoplaylists WHERE IdFaixa = ?",
+        (str(id),),
+    )
+    cursor.execute(
+        """
         DELETE FROM info
         WHERE id = (?)
-        '''
-        conn.execute(sql_excluir_faixas, (id,)) 
+        """,
+        (id,),
+    )
+    conn.commit()
+    conn.close()
