5d3c433d8399e33a2e47fea3ec93fa4d4a2104a6
abreulucas7 <abreulucas7@email.com>
2026-04-23 21:01:01 -0300

    i

--- tests/service/test_services.py
+++ tests/service/test_services.py
@@ -11,11 +11,11 @@ class TestAdicionarMusica:
         body = resp.get_json()
         assert body ["code"] == "not found"
 
-    #def test_adicionar_musica_com_campos_validos(self, mock_svc, client):
-        #mock_svc.adicionar_musica.return_value = [ post_musica( muscia = "Nome", artista = "Artista", tablatura = "Tab", letra = "Letra" )]
-        #resp = client.get("/api/musica")
-        #assert resp.status_code == 200
-        #assert resp.get_json() == [ {"muscia" : "Nome", "artista" : "Artista", "tablatura" : "Tab", "letra" : "Letra"},]
-        #mock_svc.adicionar_musica.assert_called_once()
+    def test_adicionar_musica_com_campos_validos(self, mock_svc, client):
+        mock_svc.adicionar_musica.return_value = [ post_musica( muscia = "Nome", artista = "Artista", tablatura = "Tab", letra = "Letra" )]
+        resp = client.get("/api/musica")
+        assert resp.status_code == 200
+        assert resp.get_json() == [ {"muscia" : "Nome", "artista" : "Artista", "tablatura" : "Tab", "letra" : "Letra"},]
+        mock_svc.adicionar_musica.assert_called_once()
 
 
